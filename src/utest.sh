2eaf8b94df420ef5313611f7e3e2f0b6c66543a9
Ali Dasdan <alidasdan@users.noreply.github.com>
2015-01-07 21:59:20 -0800

    reorganized directories for simplicity; added doc

--- src/utest.sh
+++ src/utest.sh
@@ -6,8 +6,8 @@
 
 for i in *.x; 
 do 
-   ./$i sample.d -v 1 | grep lambda | awk -v p=$i -v v=1 -v t=40 -v e=0.01 -f utest.awk 
-   ./$i sample.d -v 0 | grep lambda | awk -v p=$i -v v=0 -v t=50 -v e=0.01 -f utest.awk 
+   ./$i ../input/sample.d -v 1 | grep lambda | awk -v p=$i -v v=1 -v t=40 -v e=0.01 -f utest.awk 
+   ./$i ../input/sample.d -v 0 | grep lambda | awk -v p=$i -v v=0 -v t=50 -v e=0.01 -f utest.awk 
 done
 
 # EOF
